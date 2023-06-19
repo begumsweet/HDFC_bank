9c7b1ae0cce3f53951a3222c495af3a6d208a44c
admin <admin@DESKTOP-93DNEKS>
2023-06-19 17:20:32 +0530

    123456 123 123

---
+++ src/mybasics/Zzz.java
@@ -0,0 +1,19 @@
+package mybasics;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Zzz {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
